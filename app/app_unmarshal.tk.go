e01b0e411d2f7244351f9b01f57c6ac0751f321b
ikaiguang <ikaiguang@163.com>
2021-01-02 23:57:36 +0800

    app unmarshal

---
+++ app/app_unmarshal.tk.go
@@ -0,0 +1,44 @@
+package tkapp
+
+import (
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
+	tkpb "github.com/ikaiguang/srv_toolkit/api"
+	"github.com/pkg/errors"
+	"io"
+)
+
+// UnmarshalPB .
+func UnmarshalPB(buf []byte, result proto.Message) (resp *tkpb.Response, err error) {
+	resp = &tkpb.Response{}
+	err = proto.Unmarshal(buf, resp)
+	if err != nil {
+		err = errors.WithStack(err)
+		return
+	}
+
+	err = ptypes.UnmarshalAny(resp.Data, result)
+	if err != nil {
+		err = errors.WithStack(err)
+		return
+	}
+	return
+}
+
+// UnmarshalJSON .
+func UnmarshalJSON(reader io.Reader, result proto.Message) (resp *tkpb.Response, err error) {
+	resp = &tkpb.Response{}
+	err = jsonpb.Unmarshal(reader, resp)
+	if err != nil {
+		err = errors.WithStack(err)
+		return
+	}
+
+	err = ptypes.UnmarshalAny(resp.Data, result)
+	if err != nil {
+		err = errors.WithStack(err)
+		return
+	}
+	return
+}
