ee53840b3cfacc147c28011c9170aa8e9fe6de00
ikaiguang <ikaiguang@163.com>
2021-02-05 14:30:51 +0800

    添加Redis-set封装

--- redis/utils/redis_utils.tk.go
+++ redis/utils/redis_utils.tk.go
@@ -1001,3 +1001,159 @@ func RPushX(ctx context.Context, key string, value interface{}) (reply interface
 }
 
 //------------------------------------------------------------------------------
+
+// SAdd .
+// Redis Sadd 命令将一个或多个成员元素加入到集合中，已经存在于集合的成员元素将被忽略。
+// 假如集合 key 不存在，则创建一个只包含添加的元素作成员的集合。
+// 当集合 key 不是集合类型时，返回一个错误。
+// 注意：在 Redis2.4 版本以前， SADD 只接受单个成员值。
+func SAdd(ctx context.Context, key string, members ...interface{}) (reply interface{}, err error) {
+	args := make([]interface{}, 1, 1+len(members))
+	args[0] = key
+	args = appendArgs(args, members)
+	reply, err = tkredis.Redis().Do(ctx, "sadd", args...)
+	return
+}
+
+// SCard .
+// Redis Scard 命令返回集合中元素的数量。
+func SCard(ctx context.Context, key string) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "scard", key)
+	return
+}
+
+// SDiff .
+// Redis Sdiff 命令返回第一个集合与其他集合之间的差异，也可以认为说第一个集合中独有的元素。不存在的集合 key 将视为空集。
+// 差集的结果来自前面的 FIRST_KEY ,而不是后面的 OTHER_KEY1，也不是整个 FIRST_KEY OTHER_KEY1..OTHER_KEYN 的差集。
+func SDiff(ctx context.Context, keys ...interface{}) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "sdiff", keys...)
+	return
+}
+
+// SDiffStore .
+// Redis Sdiffstore 命令将给定集合之间的差集存储在指定的集合中。如果指定的集合 key 已存在，则会被覆盖。
+func SDiffStore(ctx context.Context, destination string, keys ...string) (reply interface{}, err error) {
+	args := make([]interface{}, 1+len(keys))
+	args[0] = destination
+	for i, key := range keys {
+		args[1+i] = key
+	}
+	reply, err = tkredis.Redis().Do(ctx, "sdiffstore", args...)
+	return
+}
+
+// SInter .
+// Redis Sinter 命令返回给定所有给定集合的交集。 不存在的集合 key 被视为空集。 当给定集合当中有一个空集时，结果也为空集(根据集合运算定律)。
+func SInter(ctx context.Context, keys ...interface{}) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "sinter", keys...)
+	return
+}
+
+// SInterStore .
+// Redis Sinterstore 命令将给定集合之间的交集存储在指定的集合中。如果指定的集合已经存在，则将其覆盖。
+func SInterStore(ctx context.Context, destination string, keys ...string) (reply interface{}, err error) {
+	args := make([]interface{}, 1+len(keys))
+	args[0] = destination
+	for i, key := range keys {
+		args[1+i] = key
+	}
+	reply, err = tkredis.Redis().Do(ctx, "sinterstore", args...)
+	return
+}
+
+// SIsMember .
+// Redis Sismember 命令判断成员元素是否是集合的成员。
+func SIsMember(ctx context.Context, key string, member interface{}) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "sismember", key, member)
+	return
+}
+
+// SMembers .
+// Redis Smembers 命令返回集合中的所有的成员。 不存在的集合 key 被视为空集合。
+func SMembers(ctx context.Context, key string) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "smembers", key)
+	return
+}
+
+// SMove .
+// Redis Smove 命令将指定成员 member 元素从 source 集合移动到 destination 集合。
+// SMOVE 是原子性操作。
+// 如果 source 集合不存在或不包含指定的 member 元素，则 SMOVE 命令不执行任何操作，仅返回 0 。
+// 否则， member 元素从 source 集合中被移除，并添加到 destination 集合中去。
+// 当 destination 集合已经包含 member 元素时， SMOVE 命令只是简单地将 source 集合中的 member 元素删除。
+// 当 source 或 destination 不是集合类型时，返回一个错误。
+func SMove(ctx context.Context, source, destination string, member interface{}) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "smove", source, destination, member)
+	return
+}
+
+// SPop .
+// Redis Spop 命令用于移除集合中的指定 key 的一个或多个随机元素，移除后会返回移除的元素。
+// 该命令类似 Srandmember 命令，但 SPOP 将随机元素从集合中移除并返回，而 Srandmember 则仅仅返回随机元素，而不对集合进行任何改动。
+func SPop(ctx context.Context, key string) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "spop", key)
+	return
+}
+
+// SPopN .
+// Redis Spop 命令用于移除集合中的指定 key 的一个或多个随机元素，移除后会返回移除的元素。
+// 该命令类似 Srandmember 命令，但 SPOP 将随机元素从集合中移除并返回，而 Srandmember 则仅仅返回随机元素，而不对集合进行任何改动。
+func SPopN(ctx context.Context, key string, count int64) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "spop", key, count)
+	return
+}
+
+// SRandMember .
+// Redis Srandmember 命令用于返回集合中的一个随机元素。
+// 从 Redis 2.6 版本开始， Srandmember 命令接受可选的 count 参数：
+// 如果 count 为正数，且小于集合基数，那么命令返回一个包含 count 个元素的数组，数组中的元素各不相同。如果 count 大于等于集合基数，那么返回整个集合。
+// 如果 count 为负数，那么命令返回一个数组，数组中的元素可能会重复出现多次，而数组的长度为 count 的绝对值。
+// 该操作和 SPOP 相似，但 SPOP 将随机元素从集合中移除并返回，而 Srandmember 则仅仅返回随机元素，而不对集合进行任何改动。
+func SRandMember(ctx context.Context, key string) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "srandmember", key)
+	return
+}
+
+// SRandMemberN .
+// Redis Srandmember 命令用于返回集合中的一个随机元素。
+// 从 Redis 2.6 版本开始， Srandmember 命令接受可选的 count 参数：
+// 如果 count 为正数，且小于集合基数，那么命令返回一个包含 count 个元素的数组，数组中的元素各不相同。如果 count 大于等于集合基数，那么返回整个集合。
+// 如果 count 为负数，那么命令返回一个数组，数组中的元素可能会重复出现多次，而数组的长度为 count 的绝对值。
+// 该操作和 SPOP 相似，但 SPOP 将随机元素从集合中移除并返回，而 Srandmember 则仅仅返回随机元素，而不对集合进行任何改动。
+func SRandMemberN(ctx context.Context, key string, count int64) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "srandmember", key, count)
+	return
+}
+
+// SRem .
+// Redis Srem 命令用于移除集合中的一个或多个成员元素，不存在的成员元素会被忽略。
+// 当 key 不是集合类型，返回一个错误。
+// 在 Redis 2.4 版本以前， SREM 只接受单个成员值。
+func SRem(ctx context.Context, key string, members ...interface{}) (reply interface{}, err error) {
+	args := make([]interface{}, 1, 1+len(members))
+	args[0] = key
+	args = appendArgs(args, members)
+	reply, err = tkredis.Redis().Do(ctx, "srem", args...)
+	return
+}
+
+// SUnion .
+// Redis Sunion 命令返回给定集合的并集。不存在的集合 key 被视为空集。
+func SUnion(ctx context.Context, keys ...interface{}) (reply interface{}, err error) {
+	reply, err = tkredis.Redis().Do(ctx, "sunion", keys...)
+	return
+}
+
+// SUnionStore .
+// Redis Sunionstore 命令将给定集合的并集存储在指定的集合 destination 中。如果 destination 已经存在，则将其覆盖。
+func SUnionStore(ctx context.Context, destination string, keys ...string) (reply interface{}, err error) {
+	args := make([]interface{}, 1+len(keys))
+	args[0] = destination
+	for i, key := range keys {
+		args[1+i] = key
+	}
+	reply, err = tkredis.Redis().Do(ctx, "sunionstore", args...)
+	return
+}
+
+//------------------------------------------------------------------------------
